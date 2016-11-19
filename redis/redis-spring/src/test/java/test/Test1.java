20bb21f25205983671bea6ad00c5371790c92087
ITmao <1479169142@qq.com>
2016-11-19 17:25:05 +0800

    提交redis代码

---
+++ redis/redis-spring/src/test/java/test/Test1.java
@@ -0,0 +1,24 @@
+package test;
+
+import org.junit.Test;
+
+public class Test1 {
+	public static void main(String[] args) {
+		
+	}
+	
+	@Test
+    public void getSettUnitBySettUnitIdTest() {
+        String systemId = "CES";
+        String merchantId = "133";
+//        SettUnit configSettUnit = settUnitService.getSettUnitBySettUnitId(systemId, merchantId, "ESP");
+//        SettUnit configSettUnit1 = settUnitService.getSettUnitBySettUnitId(systemId, merchantId, "ESP");
+//        boolean flag= (configSettUnit == configSettUnit1);
+//        System.out.println(configSettUnit);
+//        logger.info("查找结果" + configSettUnit.getBusinessType());
+
+      //  localSecondFIFOCache.put("configSettUnit", configSettUnit.getBusinessType());
+     //  String string = localSecondFIFOCache.get("configSettUnit");
+//        logger.info("查找结果" + string);
+    }
+}
